42a65eaefaad81efec95228b0bf50030d7b7503b
Júnior Palitot <antonio.junior@tecw.com.br>
2017-01-19 13:41:49 -0300

    fim thread le

--- src/socketteste/ThreadLe.java
+++ src/socketteste/ThreadLe.java
@@ -40,7 +40,7 @@ public class ThreadLe extends Thread {
             } catch (IOException ex) {
                 Logger.getLogger(ThreadLe.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }while(!msg.equals("fim"));
+        }while(!msg.equals("bye"));
     }
     
 }
